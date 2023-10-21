daa1a5b82aaf1988810a413bee826583bf478b44
Hecoal <hectorcortez_2002@hotmail.com>
2023-10-20 20:43:04 -0600

    Added Middlewares & Routes

---
+++ Routes/employees.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const employees = express.Router();
+const db = require('../Config/database');
+
+
+module.exports=employees;
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const morgan = require ('morgan');
 const express = require ('express');
 const app = express();
 
-app.get('/',(req,res,next)=>{
-    res.status(200);
-    res.send('WELCOME');
-});
+//Routes
+const employees = require('./Routes/employees.js');
+
+//Middleware
+const notFound = require('./Middleware/notFound.js');
+
+app.use(morgan('dev'));
 
 app.listen(3000, ()=>{
     console.log('Server is running');
