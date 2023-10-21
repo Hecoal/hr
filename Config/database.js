eb822b5fd114f690274d37ce2e3ed9ca7a13f7e4
Hecoal <hectorcortez_2002@hotmail.com>
2023-10-20 20:27:05 -0600

    Added pool for database

--- Config/database.js
+++ Config/database.js
@@ -1,3 +1,6 @@
+const mysql= require ('mysql');
+const util = require('util');
+
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
@@ -5,3 +8,6 @@ const pool = mysql.createPool({
     password: '',
     database:'hr'
 });
+pool.query=util.promisify(pool.query);
+
+module.exports =pool;
