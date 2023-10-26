9a5d1969ae851cbf615310955b519766f06c62b0
Hecoal <hectorcortez_2002@hotmail.com>
2023-10-25 18:55:47 -0600

    Added function to add an employee

--- HR/js/addEmployee.js
+++ HR/js/addEmployee.js
@@ -1,23 +1,37 @@
+window.onload=init;
+var headers ={};
+var url='http://localhost:3000';
 
-function singin(){
+function init(){
+    if(localStorage.getItem('token')){
+        headers={
+            headers:{
+                'Authorization': 'bearer ' + localStorage.getItem('token')
+            }
+        }
+        document.querySelector('.btn-primary').addEventListener('click',addEmployee);
+
+    }else{
+        window.location.href='index.html';
+    }
+}
+
+function addEmployee(){
     var name= document.getElementById('input-name').value;
     var lastname = document.getElementById('input-lastname').value;
     var phone = document.getElementById('input-phone').value;
     var mail = document.getElementById('input-mail').value;
     var address= document.getElementById('input-address').value;
-
-    //Send data to server
-    axios({
-        method: 'post',
-        url: 'http://localhost:3000/users/signin',
-        data: {
+    
+    var postData={
             name:name,
             lastName:lastname,
             phoneNumber:phone,
             email:mail,
             address:address
-        }
-    }).then(function(res){
+    }
+    axios.post(url + '/employees', postData, headers)
+    .then(function(res){
         console.log(res);
         $('.alert-success').show();
     }).catch(function(err){
